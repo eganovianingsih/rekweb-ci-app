16148418679c9f5d39599889056bf3572a0bff53
eganovianingsih <eganovia07@mail.unpas.ac.id>
2019-10-19 22:30:32 +0700

    menambahkan folder ci-app dimana terdapat banyak file yang akan memanggil dan menampilkan data yang ada didalam database phpmvc

---
+++ application/views/mahasiswa/index.php
@@ -0,0 +1,14 @@
+<div class="container">
+	
+
+	<div class="row">
+		<div class="col-md-3">
+			<h3>Daftar Mahasiswa</h3>
+			<ul class="list-group">
+			  <?php foreach( $mahasiswa as $mhs ) : ?>
+			  	<li class="list-group-item"><?= $mhs['nama']; ?></li>
+			  <?php endforeach; ?>
+			</ul>
+		</div>
+	</div>
+</div>
