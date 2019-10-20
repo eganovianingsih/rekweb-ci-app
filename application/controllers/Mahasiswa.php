7668f8f9d7aa9ef4e47c926e08897ba9b380d1e9
eganovianingsih <eganovia07@mail.unpas.ac.id>
2019-10-20 15:38:03 +0700

    menambahkan fiture tambah dan hapus data, dan juga menambahkan form,form validation dan flashdata

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -1,11 +1,12 @@
 <?php 
 
 	class Mahasiswa extends CI_Controller {
+
 		public function __construct()
 			{
 				parent::__construct();
 			$this->load->model('Mahasiswa_model');
-			
+			$this->load->library('form_validation');
 			}
 
 		public function index()
@@ -17,13 +18,36 @@
 			$this->load->view('mahasiswa/index',$data);
 			$this->load->view('templates/footer');
 			
-			
 		}
 
-		// public function tambah()
-		// {
-		// 	$this->load
-		// }
+
+		public function tambah()
+		{
+			$data['judul'] = 'Form Tambah Data Mahasiswa';
+
+			$this->form_validation->set_rules('nama','Nama', 'required');
+			$this->form_validation->set_rules('nrp','NRP', 'required|numeric');
+			$this->form_validation->set_rules('email','Email', 'required|valid_email');
+
+			if( $this->form_validation->run()== FALSE){
+			$this->load->view('templates/header',$data);
+			$this->load->view('mahasiswa/tambah');
+			$this->load->view('templates/footer');
+		} else {
+			$this->Mahasiswa_model->tambahDataMahasiswa();
+			$this->session->set_flashdata('flash', 'Ditambahakan');
+			redirect('mahasiswa');
+		}
+
+		}
+
+		public function hapus($id)
+		{
+			$this->Mahasiswa_model->hapusDataMahasiswa($id);
+			$this->session->set_flashdata('flash','dihapus');
+			redirect('mahasiswa');
+		}
+
 	}
 
  ?>
--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -6,6 +6,25 @@
 			return $this->db->get('mahasiswa')->result_array();
 			
 		}
+
+		public function tambahDataMahasiswa()
+		{
+				$data = [
+					"nama" => $this->input->post('nama', true),
+					"nrp" => $this->input->post('nrp', true),
+					"email" => $this->input->post('email', true),
+					"jurusan" => $this->input->post('jurusan')
+				];
+
+				
+				$this->db->insert('mahasiswa', $data);
+		}
+
+		public function hapusDataMahasiswa($id)
+		{
+			$this->db->where('id',$id);
+			$this->db->delete('mahasiswa');
+		}
 	}
 
  ?>
