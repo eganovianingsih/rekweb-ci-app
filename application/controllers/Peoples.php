bce1cc5749da68de775430972dd415109ebcd1a6
eganovianingsih <eganovia07@mail.unpas.ac.id>
2019-10-22 18:39:20 +0700

    menambahkan fitur SEARCHING dan PAGINATION

--- application/controllers/Peoples.php
+++ application/controllers/Peoples.php
@@ -24,6 +24,7 @@
 			//config
 
 			$this->db->like('name', $data['keyword']);
+			$this->db->or_like('email', $data['keyword']);
 			$this->db->from('peoples');
 			$config['total_rows'] = $this->db->count_all_results();
 			$data['total_rows'] = $config['total_rows'];
